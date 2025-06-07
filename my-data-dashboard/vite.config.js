e4d61d22a6a7d2ac7bf654d07090c224302fb629
happy-sw <1005674130@qq.com>
2025-06-07 14:46:01 +0800

    vite配置

--- my-data-dashboard/vite.config.js
+++ my-data-dashboard/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 插件配置
   plugins: [vue()],
-})
+
+  //模块解析配置
+  resolve: {
+    alias: {
+      "@": "./src",
+    },
+  },
+  // 开发服务器配置
+  server: {
+    hmr: true,
+  },
+});
