e0bfe37b699969f3c97d0190813be7e96881a66a
happy-sw <1005674130@qq.com>
2025-06-08 10:38:35 +0800

    顶部图标

--- my-data-dashboard/src/components/title/index.vue
+++ my-data-dashboard/src/components/title/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>北京银行智能合规中心</div>
+  <div>
+    <div class="header-title">北京银行智能合规中心</div>
+    <div class="header-number"></div>
+    <div class="header-time"></div>
+    <div class="header-weather"></div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.header-title {
+  font-size: 30px;
+  font-weight: 700;
+  color: #deecff;
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
