476d1eda73453fdcdb93f52f8b7669f667df8e2a
happy-sw <1005674130@qq.com>
2025-07-19 23:49:53 +0800

    新增中部的矩形卡片和圆环组件，和表格

---
+++ my-data-dashboard/src/components/infoCard/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="info-card">
+      <div class="title">{{ title }}</div>
+      <div class="description">{{ description }}</div>
+    </div>
+    <div class="info-card">
+      <div class="title">{{ title }}</div>
+      <div class="description">{{ description }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: "主题名称",
+  },
+  description: {
+    type: String,
+    default: "这里是描述内容",
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.info-card {
+  background: linear-gradient(135deg, #80ffa5, #37a2ff); /* ✅ 渐变 */
+  border-radius: 16px;
+  padding: 16px 24px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  color: #003344;
+  width: 23%;
+  font-size: 10px;
+  max-width: 400px;
+  box-sizing: border-box;
+}
+
+.title {
+  font-weight: bold;
+  font-size: 10px;
+  margin-bottom: 8px;
+}
+
+.description {
+  font-size: 10px;
+  opacity: 0.8;
+}
+</style>
--- my-data-dashboard/src/views/centerContent/index.vue
+++ my-data-dashboard/src/views/centerContent/index.vue
@@ -4,7 +4,23 @@
       <div>
         <WaveChart></WaveChart>
       </div>
-      <div>2</div>
+      <div class="center_info">
+        <!-- 左侧区域 -->
+        <div class="center_info_card">
+          <!-- 上半部分 -->
+          <div class="center_info_card_top">
+            <InfoCard></InfoCard>
+          </div>
+          <!-- 下半部分 -->
+          <div class="center_info_card_bottom">
+            <CountUp></CountUp>
+          </div>
+        </div>
+        <!-- 右侧区域 -->
+        <div class="center_info_search">
+          <SearchTable></SearchTable>
+        </div>
+      </div>
       <div>3</div>
     </div>
   </div>
@@ -12,6 +28,36 @@
 
 <script setup>
 import WaveChart from "@/components/waveChart/index.vue";
+import InfoCard from "@/components/infoCard/index.vue";
+import CountUp from "@/components/countUp/index.vue";
+import SearchTable from "@/components/searchTable/index.vue";
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.center_info {
+  border: 1px solid red;
+  display: flex;
+  box-sizing: border-box;
+
+  .center_info_card {
+    border: 1px solid red;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    .center_info_card_top {
+      flex: 1;
+    }
+    .center_info_card_bottom {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+  .center_info_search {
+    border: 1px solid red;
+    flex: 1;
+  }
+}
+</style>
