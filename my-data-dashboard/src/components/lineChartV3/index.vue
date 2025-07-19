2eb1c4d50a9782dd02e9c44bbe3fec4ca52c5bdd
happy-sw <1005674130@qq.com>
2025-07-19 22:27:43 +0800

    新增右侧中部堆叠折线图

--- my-data-dashboard/src/components/lineChartV3/index.vue
+++ my-data-dashboard/src/components/lineChartV3/index.vue
@@ -33,6 +33,7 @@ use([
 const days = ["周一", "周二", "周三", "周四", "周五", "周六", "周日"];
 const newUsers = [120, 200, 150, 80, 70, 110, 130];
 const orders = [300, 450, 500, 400, 600, 700, 750];
+const sum = [445, 521, 954, 348, 580, 810, 980];
 
 const option = ref({
   title: {
@@ -46,7 +47,7 @@ const option = ref({
     trigger: "axis",
   },
   legend: {
-    data: ["新增用户", "订单数量"],
+    data: ["新增用户", "订单数量", "总数据量"],
     textStyle: {
       color: "#fff",
     },
@@ -76,30 +77,56 @@ const option = ref({
       axisLine: { lineStyle: { color: "#fff" } },
       axisLabel: { color: "#fff" },
     },
+    {
+      type: "value",
+      name: "总数据量",
+      axisLine: { lineStyle: { color: "#fff" } },
+      axisLabel: { color: "#fff" },
+    },
   ],
   series: [
     {
       name: "新增用户",
-      type: "bar",
+      type: "line",
       data: newUsers,
-      barWidth: 10,
+      smooth: true,
+      lineStyle: {
+        color: "#00DDFF",
+        width: 0,
+      },
+
       itemStyle: {
         color: "#00DDFF",
         borderRadius: [10, 10, 0, 0], //设置每个圆柱的圆角大小
       },
+      areaStyle: {
+        // ✅ 添加渐变阴影
+        color: {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            { offset: 0, color: "rgb(1, 191, 236)" }, // 顶部
+            { offset: 1, color: "rgba(250, 204, 20, 0)" }, // 底部透明
+          ],
+        },
+        shadowColor: "rgba(0, 0, 0, 0.3)", // 可选阴影
+        shadowBlur: 10,
+      },
     },
     {
-      name: "订单数量",
+      name: "总数据量",
       type: "line",
       yAxisIndex: 1,
-      data: orders,
+      data: sum,
       smooth: true,
       lineStyle: {
         color: "#80FFA5",
-        width: 3,
+        width: 0,
       },
-      symbol: "circle",
-      symbolSize: 8,
+
       itemStyle: {
         color: "#80FFA5",
       },
@@ -113,6 +140,37 @@ const option = ref({
           y2: 1,
           colorStops: [
             { offset: 0, color: "rgb(128, 255, 165)" }, // 顶部
+            { offset: 1, color: "rgba(250, 204, 20, 0)" }, // 底部透明
+          ],
+        },
+        shadowColor: "rgba(0, 0, 0, 0.3)", // 可选阴影
+        shadowBlur: 10,
+      },
+    },
+    {
+      name: "",
+      type: "line",
+      yAxisIndex: 1,
+      data: orders,
+      smooth: true,
+      lineStyle: {
+        color: "#4D77FF",
+        width: 0,
+      },
+
+      itemStyle: {
+        color: "#4D77FF",
+      },
+      areaStyle: {
+        // ✅ 添加渐变阴影
+        color: {
+          type: "linear",
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            { offset: 0, color: "rgb(77, 119, 255)" }, // 顶部
             { offset: 1, color: "rgba(250, 204, 20, 0)" }, // 底部透明
           ],
         },
--- my-data-dashboard/src/views/rightContent/index.vue
+++ my-data-dashboard/src/views/rightContent/index.vue
@@ -3,13 +3,16 @@
     <div>
       <BarChart></BarChart>
     </div>
-    <div>2</div>
+    <div>
+      <LineChartV3></LineChartV3>
+    </div>
     <div>3</div>
   </div>
 </template>
 
 <script setup>
 import BarChart from "@/components/barChart/index.vue";
+import LineChartV3 from "@/components/lineChartV3/index.vue";
 </script>
 
 <style scoped lang="scss"></style>
